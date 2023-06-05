1474206e2e8e45c685cd9b133f022b8756b11c69
Nak <leoyang891129@gmail.com>
2023-06-05 23:46:39 +0800

    put 語法

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.get("/students", async (req, res) => {
   }
 });
 
+app.get("/students/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundStudent = await Students.find({ _id }).exec();
+    return res.send(foundStudent);
+  } catch (e) {
+    return res.status(500).send("尋找資料時錯誤");
+  }
+});
+
 //因為要用post 新增   app.use(express.json); app.use(express.urlencoded({ extended: true }));
 app.post("/students", async (req, res) => {
   try {
@@ -52,6 +62,22 @@ app.post("/students", async (req, res) => {
   }
 });
 
+app.put("/students/:_id", async (req, res) => {
+  try {
+    let { _id } = req.params;
+    let { name, age, major, merit, other } = req.body;
+    let newData = await Student.findOneAndUpdate(
+      { _id },
+      { name, age, major, merit, other },
+      { new: true, runValidators: true, overwrite: true }
+      //因為HTTP put request要求客戶端提供所有數據，所以
+      //我們需要根據客戶端提供的數據，來更新資料庫內的資料
+    );
+    res.send({ msg: "成功更新學生資料!", updateData: newData });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 app.listen(3000, () => {
   console.log("3000正在執行");
 });
