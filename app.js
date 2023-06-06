44fb9617dc4b58eae44536d583a0ef7cd988e1ff
Nak <leoyang891129@gmail.com>
2023-06-06 11:23:05 +0800

    下載cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Students = require("./models/student");
 const Student = require("./models/student");
-
+const cors = require("cors");
 mongoose
   .connect("mongodb://127.0.0.1/testdb")
   .then(() => {
@@ -17,6 +17,7 @@ mongoose
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/students", async (req, res) => {
   try {
