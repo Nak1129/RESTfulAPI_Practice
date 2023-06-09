e3a7f8f2a029862a00344654d2e2e5f062de29ea
Nak <leoyang891129@gmail.com>
2023-06-09 18:57:42 +0800

    透過網頁新增、更新學生資料 ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Students = require("./models/student");
 const Student = require("./models/student");
 const cors = require("cors");
+const methodOverride = require("method-override");
 mongoose
   .connect("mongodb://127.0.0.1/testdb")
   .then(() => {
@@ -18,23 +19,50 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(methodOverride("_method"));
 
 app.get("/students", async (req, res) => {
   try {
     let studentData = await Students.find({}).exec();
-    return res.send(studentData);
+    //return res.send(studentData);
+
+    return res.render("students", { studentData }); //render(哪個.ejs,物件)
   } catch (e) {
     return res.status(500).send("尋找資料時錯誤");
   }
 });
 
+app.get("/students/new", (req, res) => {
+  return res.render("student-new-form");
+});
+
 app.get("/students/:_id", async (req, res) => {
   let { _id } = req.params;
   try {
-    let foundStudent = await Students.find({ _id }).exec();
-    return res.send(foundStudent);
+    let foundStudent = await Students.findOne({ _id }).exec();
+
+    //return res.send(foundStudent);
+    if (foundStudent != null) {
+      return res.render("student-page", { foundStudent });
+    } else {
+      return res.status(400).render("student-not-found");
+    }
   } catch (e) {
-    return res.status(500).send("尋找資料時錯誤");
+    return res.status(400).render("student-not-found");
+  }
+});
+
+app.get("/students/:_id/edit", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundStudent = await Student.findOne({ _id }).exec();
+    if (foundStudent != null) {
+      return res.render("edit-student", { foundStudent });
+    } else {
+      return res.status(400).render("student-not-found");
+    }
+  } catch (e) {
+    return res.status(400).render("student-not-found");
   }
 });
 
@@ -54,12 +82,9 @@ app.post("/students", async (req, res) => {
     });
     let saveStudent = await newStudent.save();
     //把send做成一個物件
-    return res.send({
-      msg: "資料儲存成功",
-      savedObject: saveStudent,
-    });
+    return res.render("student-save-success", { saveStudent });
   } catch (e) {
-    return res.status(400).send("儲存資料時錯誤");
+    return res.status(400).render("student-save-fall");
   }
 });
 
@@ -69,14 +94,14 @@ app.put("/students/:_id", async (req, res) => {
     let { name, age, major, merit, other } = req.body;
     let newData = await Student.findOneAndUpdate(
       { _id },
-      { name, age, major, merit, other },
+      { name, age, major, scholarship: { merit, other } },
       { new: true, runValidators: true, overwrite: true }
       //因為HTTP put request要求客戶端提供所有數據，所以
       //我們需要根據客戶端提供的數據，來更新資料庫內的資料
     );
-    res.send({ msg: "成功更新學生資料!", updateData: newData });
+    return res.render("student-update-success", { newData });
   } catch (e) {
-    return res.status(400).send(e);
+    return res.status(400).render("student-update-fall");
   }
 });
 
